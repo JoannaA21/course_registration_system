0f50c465acc6ea8c71686b7888b66f70ad812ec7
Jozel Surro <112725152+Jozelsurro2301@users.noreply.github.com>
2023-10-18 13:45:32 -0600

    Phase1 jozel (#2)
    * Admin Page
    
    * added css

--- a1/src/components/Admin.js
+++ a1/src/components/Admin.js
@@ -1,7 +1,97 @@
-export const Hi = () => {
-    return(
-        <>
-            <h1> I am admin page</h1>
-        </>
+import AddCourse from "./AddCourse"
+import Course from "./Course"
+import SearchCourse from "./SearchCourse"
+import { useState } from "react"
+import "../css/admin.css"
+
+
+export const Admin = () => {
+
+//Courses Data to be Displayed
+const [courses, setCourse] = useState(JSON.parse(localStorage.getItem('ListofCourses') || []));
+
+//Initial reset data for Add Course Forms
+const initialNewCourse = {
+  code: '',
+  title: '',
+  startdate: '',
+  enddate: '',
+  days: '',
+  starttime: '',
+  endtime: '',
+  instructor: ''
+};
+
+//State for new Data to be push to the courses array Object
+const [newCourse, setNewCourse] = useState(initialNewCourse);
+
+//State for the search Form
+const [searchCourse, setSearch] = useState('');
+
+//Function that pushes the new data to the courses array object and stores the data on the local storage
+const setAndSaveCourse = (newCourse) => {
+  setCourse(newCourse);
+  localStorage.setItem('ListofCourses', JSON.stringify(newCourse));
+}
+
+//Handles Changes on the Form
+const handleChange=(e)=>{      
+  const name = e.target.name;
+  const value = e.target.value;
+  setNewCourse((curCourses)=> 
+    ({...curCourses, [name]: value}));      
+}
+
+//Handles the delete
+const handleDelete = (id) => {
+    const listcourses = courses.filter((course)=> course.id !== id); 
+    setAndSaveCourse(listcourses);
+}
+
+//Handles the submit on Add course Form
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const id = courses.length ? courses[courses.length - 1].id + 1 : 1;
+  const newCourseItem = { id, ...newCourse };
+  const updatedCourses = Array.isArray(courses) ? [...courses, newCourseItem] : [newCourseItem];;
+  setAndSaveCourse(updatedCourses);
+
+  setNewCourse(initialNewCourse);
+};
+  
+
+    return (
+        <div className="adminpage">
+            <AddCourse
+                courses={courses}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                newCourse ={newCourse}
+            />
+
+            <SearchCourse
+            searchCourse = {searchCourse}
+            setSearch = {setSearch}
+            />
+
+            {/* Displaying Courses */}
+
+            {courses.length ? (
+                <Course
+                    courses={courses.filter(course =>
+                                            course.code.toLowerCase().includes(searchCourse.toLowerCase()) ||
+                                            course.title.toLowerCase().includes(searchCourse.toLowerCase())
+                                    )}
+                    handleDelete={handleDelete}
+                />
+            ) : (
+                <p style={{
+                    fontSize:'2rem',
+                    fontWeight:'bolder',
+                    display:'flex',
+                    justifyContent: 'center',
+                    margin: '4rem'}}>There are no available Courses</p>
+            )}
+        </div>
     )
 }
---
+++ a1/src/components/Course.js
@@ -0,0 +1,21 @@
+const Course = ({courses, handleDelete}) => {
+    //Maps through all the courses data and displays it
+    return (
+        <ul>
+            {courses.map((course) => (
+                <li className="displayCourseContainer" key={course.id}>
+                    <h2 className="displayCourseH2">{course.code} : {course.title}</h2>
+                    <p className="displayCourseDetails">Course Start Date : {course.startdate}</p>
+                    <p className="displayCourseDetails">Course End Date :  {course.enddate}</p>
+                    <p className="displayCourseDetails">Course Schedule : {course.days}</p>
+                    <p className="displayCourseDetails">Course Time : {course.starttime} - {course.endtime}</p>
+                    <p className="displayCourseDetails">Course Instructor : {course.instructor}</p>
+                    <button className="displayCourseRegister" onClick={ ()=> handleDelete(course.id)}>Delete</button>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+
+export default Course
